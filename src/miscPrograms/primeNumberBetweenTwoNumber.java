4fa6ac03d8a708cda19a941c285ab786276edff5
bijender prasad <bijenderprasad07@gmail.com>
2021-05-18 17:48:27 +0530

    checking head@ values again

--- src/miscPrograms/primeNumberBetweenTwoNumber.java
+++ src/miscPrograms/primeNumberBetweenTwoNumber.java
@@ -3,30 +3,30 @@ package miscPrograms;
 public class primeNumberBetweenTwoNumber {
 
 	public static void main(String[] args) {
-	int start = 1;
-	int stop = 100;
-	isPrimeNumber(99);
-	findPrimeNumberBetweenTwoNumber(start,stop);
+		int start = 1;
+		int stop = 100;
+		isPrimeNumber(99);
+		findPrimeNumberBetweenTwoNumber(start, stop);
 	}
-	
+
 	public static void isPrimeNumber(int num) {
-		int half = num/2;
-		for (int i = 2 ; i <=half ; i++) {
-			if (num%i==0) {
+		int half = num / 2;
+		for (int i = 2; i <= half; i++) {
+			if (num % i == 0) {
 				System.out.println("Number is not a prime number");
 				break;
-							}
 			}
-		
-				System.out.println("Number is a prime number");
-				}
-	
+		}
+
+		System.out.println("Number is a prime number");
+	}
+
 	public static void findPrimeNumberBetweenTwoNumber(int start, int stop) {
-		int count = 0; 
+		int count = 0;
 
 		for (int i = start; i <= stop; i++) {
 			int flag = 1;
-			for (int j = 2; j <= start/2; j++) {
+			for (int j = 2; j <= start / 2; j++) {
 				if (start % j == 0) {
 					// System.out.println(start+ " is not a prime number");
 					flag = 0;
@@ -34,13 +34,13 @@ public class primeNumberBetweenTwoNumber {
 				}
 
 			}
-			if (flag == 1 && start!=1) {
+			if (flag == 1 && start != 1) {
 				System.out.println(start + " is prime number");
 				count++;
 			}
 			start++;
 		}
-		System.out.println("Total prime within the specified range is: "+count);
+		System.out.println("Total prime within the specified range is: " + count);
 
 	}
 
