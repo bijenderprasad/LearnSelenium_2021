9c0c7479d2caa57bc45ff1b99ec7ce8a6d115878
bijender prasad <bijenderprasad07@gmail.com>
2021-05-18 17:37:58 +0530

    prime numebrs

--- src/StringsProgram/stringCount.java
+++ src/StringsProgram/stringCount.java
@@ -3,7 +3,31 @@ package StringsProgram;
 public class stringCount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "bijender";
+		findOutTheStringLength(name);
+		System.out.println(getLengthOfString(name));
+	}
+
+	public static void findOutTheStringLength(String name) {
+		int stringlength = 0;
+		char[] count = name.toCharArray();
+		for (int x : count) {
+			stringlength++;
+		}
+		System.out.println(stringlength);
+	}
+
+	public static int getLengthOfString(String name) {
+		int i = 0;
+		try {
+			for (i=0;; i++) {
+				name.charAt(i);
+			}
+
+		} catch (Exception e) {
+
+		}
+		return i;
 
 	}
 
---
+++ src/miscPrograms/primeNumberBetweenTwoNumber.java
@@ -0,0 +1,46 @@
+package miscPrograms;
+
+public class primeNumberBetweenTwoNumber {
+
+	public static void main(String[] args) {
+	int start = 1;
+	int stop = 100;
+	findPrimeNumberBetweenTwoNumber(start,stop);
+	}
+	
+	public static void isPrimeNumber(int num) {
+		int half = num/2;
+		for (int i = 2 ; i <=half ; i++) {
+			if (num%i==0) {
+				System.out.println("Number is not a prime number");
+				break;
+							}
+			}
+		
+				System.out.println("Number is a prime number");
+				}
+	
+	public static void findPrimeNumberBetweenTwoNumber(int start, int stop) {
+		int count = 0; 
+
+		for (int i = start; i <= stop; i++) {
+			int flag = 1;
+			for (int j = 2; j <= start/2; j++) {
+				if (start % j == 0) {
+					// System.out.println(start+ " is not a prime number");
+					flag = 0;
+					break;
+				}
+
+			}
+			if (flag == 1 && start!=1) {
+				System.out.println(start + " is prime number");
+				count++;
+			}
+			start++;
+		}
+		System.out.println("Total prime within the specified range is: "+count);
+
+	}
+
+}
